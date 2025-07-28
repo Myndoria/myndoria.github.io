fe0b3fed109268aec6b4ca6089e686f2331f6821
Myndoria <Myndoria@yahoo.com>
2025-07-28 16:56:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
-document.getElementById("darkModeToggle").addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
+document.getElementById("toggle-theme").addEventListener("click", () => {
+  const html = document.documentElement;
+  const currentTheme = html.getAttribute("data-theme");
+  const nextTheme = currentTheme === "dark" ? "light" : "dark";
+  html.setAttribute("data-theme", nextTheme);
 });
