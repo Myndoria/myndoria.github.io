b6174d474538999dc934c28792f8094ec2ac32ef
Myndoria <Myndoria@yahoo.com>
2025-07-30 04:26:26 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("toggle-theme").addEventListener("click", () => {
-  const html = document.documentElement;
-  const currentTheme = html.getAttribute("data-theme");
-  const nextTheme = currentTheme === "dark" ? "light" : "dark";
-  html.setAttribute("data-theme", nextTheme);
-});
