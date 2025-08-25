4bb90e67e97e6357c3b66cfee82770213779a216
Myndoria <Myndoria@yahoo.com>
2025-08-25 05:40:13 -0400

    Delete script.js.js

--- script.js.js
+++
@@ -1,7 +0,0 @@
-// Dark mode toggle
-document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.getElementById("darkModeToggle");
-  toggle?.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-  });
-});
